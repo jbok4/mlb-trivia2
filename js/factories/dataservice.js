1852d7959351b0077d829a2e6b62d45e09b9a551
jbok4 <jbok4@aol.com>
2017-01-04 16:55:16 -0500

    update of data

--- js/factories/dataservice.js
+++ js/factories/dataservice.js
@@ -213,22 +213,22 @@
             type: "text",
             text: "Before Jacob deGrom won it in 2014, who was the last Met to win the NL Rookie of the Year?",
             possibilities: [
-                {                    answer: "Tom Seaver"                },
-                {                    answer: "Jon Matlack"                 },
-                {                    answer: "Darryl Strawberry"                 },
-                {                    answer: "Dwight Gooden"                 }
+                {    answer: "Tom Seaver"          },
+                {    answer: "Jon Matlack"          },
+                {    answer: "Darryl Strawberry"      },
+                {    answer: "Dwight Gooden"         }
             ],
             selected: null,
             correct: null
         },
         {
             type: "image",
-            text: "Which of these is the Olive Ridley Turtle?",
+            text: "Which of these pitchers never pitched a no-hitter?",
             possibilities: [
-                {                    answer: "http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg"                },
-                {                    answer: "http://assets.worldwildlife.org/photos/163/images/carousel_small/SCR_290360hawskbill-why-matter-LG.jpg?1345565532"                },
-                {                    answer: "http://images.nationalgeographic.com/wpf/media-live/photos/000/006/cache/ridley-sea-turtle_688_600x450.jpg"                },
-                {                    answer: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Kemp's_Ridley_sea_turtle_nesting.JPG"                }
+                {   answer: "https://shlabotnikreport.files.wordpress.com/2013/12/1986-topps-box-cards-dwight-gooden.jpg"                },
+                {   answer: "http://my.execpc.com/~pjsports/CONE/89AS9.jpg"                },
+                {   answer: "http://ep.yimg.com/ay/tbcs/2013-topps-factory-set-orange-johan-santana-baseball-card-2.gif"                },
+                {   answer: "http://www.tradingcarddb.com/Images/Cards/Baseball/107/107-77Fr.jpg"                }
             ],
             selected: null,
             correct: null
@@ -261,31 +261,31 @@
 
     var playersData = [
         {
-            type: "Green Turtle",
-            image_url: "http://www.what-do-turtles-eat.com/wp-content/uploads/2014/10/Sea-Turtles-Habitat.jpg",
-            locations: "Tropical and subtropical oceans worldwide",
+            type: "1969 World Series",
+            image_url: "https://images-na.ssl-images-amazon.com/images/I/71F-hwl5-oL._SY355_.jpg",
+            locations: "Shea Stadium and Camden Yards",
             size: "Up to 1.5m and up to 300kg",
             lifespan: "Over 80 years",
             diet: "Herbivore",
-            description: "The green turtle is a large, weighty sea turtle with a wide, smooth carapace, or shell. It inhabits tropical and subtropical coastal waters around the world and has been observed clambering onto land to sunbathe. It is named not for the color of its shell, which is normally brown or olive depending on its habitat, but for the greenish color of its skin. There are two types of green turtles—scientists are currently debating whether they are subspecies or separate species—including the Atlantic green turtle, normally found off the shores of Europe and North America, and the Eastern Pacific green turtle, which has been found in coastal waters from Alaska to Chile."
+            description: "The laughingstocks of baseball for much of the 1960s, the New York Mets perform a rapid and dramatic turnaround, capturing the hearts of fans everywhere with an eye-popping World Series performance against the Baltimore Orioles. Pitching was the undeniable strength of the 1969 Mets. Tom Seaver, appropriately earning his nickname 'Terrific Tom,' won his last ten decisions to finish at 25-7 with a 2.21 earned run average. Koosman won his last five to end the year at 17-9 and a 2.28 ERA. A sharp bullpen was anchored by Tug McGraw and Ron Taylor. Nolan Ryan, limited by groin problems, won six of nine decisions and struck out 92 batters in 89 innings."
         },
         {
-            type: "Loggerhead Turtle",
-            image_url: "http://i.telegraph.co.uk/multimedia/archive/02651/loggerheadTurtle_2651448b.jpg",
-            locations: "Tropical and subtropical oceans worldwide",
+            type: "1986 World Series",
+            image_url: "https://upload.wikimedia.org/wikipedia/en/c/c7/1986_World_Series.gif",
+            locations: "Shea Stadium and Fenway Park",
             size: "90cm, 115kg",
-            lifespan: "More than 50 years",
+            lifespan: "Ray Knight",
             diet: "Carnivore",
-            description: "Loggerhead turtles are the most abundant of all the marine turtle species in U.S. waters. But persistent population declines due to pollution, shrimp trawling, and development in their nesting areas, among other factors, have kept this wide-ranging seagoer on the threatened species list since 1978. Their enormous range encompasses all but the most frigid waters of the world's oceans. They seem to prefer coastal habitats, but often frequent inland water bodies and will travel hundreds of miles out to sea."
+            description: "The 1986 World Series pitted the National League champions New York Mets against the American League champions Boston Red Sox. The Mets, coached by Bud Harrelson won the Series in the seventh game, after overcoming a nearly hopeless deficit in Game 6 — a game in which the Red Sox were twice one strike away from victory and that ended on a famous error by Boston's first baseman Bill Buckner after their lead had already been blown. Game 6 has been cited in the legend of the Curse of the Bambino to explain the major comeback. The Mets entered the World Series as big favorites, having won 108 games during the regular season."
         },
         {
-            type: "Leatherback Turtle",
-            image_url: "https://static-secure.guim.co.uk/sys-images/Guardian/Pix/pictures/2011/8/13/1313246505515/Leatherback-turtle-007.jpg",
-            locations: "All tropical and subtropical oceans",
+            type: "NL Rookies of the Year",
+            image_url: "http://mediadownloads.mlb.com/mlbam/2014/11/13/images/mlbf_36910429_th_45.jpg",
+            locations: "National League",
             size: "Up to 2m, up to 900kg",
             lifespan: "45 years",
             diet: "Carnivore",
-            description: "Leatherbacks are the largest turtles on Earth, growing up to seven feet (two meters) long and exceeding 2,000 pounds (900 kilograms). These reptilian relics are the only remaining representatives of a family of turtles that traces its evolutionary roots back more than 100 million years. Once prevalent in every ocean except the Arctic and Antarctic, the leatherback population is rapidly declining in many parts of the world. While all other sea turtles have hard, bony shells, the inky-blue carapace of the leatherback is somewhat flexible and almost rubbery to the touch. Ridges along the carapace help give it a more hydrodynamic structure. Leatherbacks can dive to depths of 4,200 feet (1,280 meters)—deeper than any other turtle—and can stay down for up to 85 minutes."
+            description: "The Baseball Writers Association of America honors the outstanding rookie player in each league with the Rookie of the Year Award. The four Mets rookies who have won the award are Tom Seaver in 1967, Jon Matlack in 1972, Darryl Strawberry in 1983, and Dwight Gooden in 1984."
         },
         {
             type: "Hawksbill Sea Turtle",
